5d62c7c0a2a265997b34f62a6ea925811f3a6372
Mohammad Faries <schwyzries@gmail.com>
2022-03-24 12:17:54 +0800

    new file

--- Documents/UTeM/SEM 4/BENR 2423 database and cloud/coding/mongodb.js
+++ Documents/UTeM/SEM 4/BENR 2423 database and cloud/coding/mongodb.js
@@ -12,6 +12,15 @@ client.connect(err => {
   }
   console.log("Connected to mongodb");
 
+  let result = client.db('Newdatabase').collection('customer').insertOne({
+    name: 'Ronny Kessler',
+    city: 'Overland Park',
+    avatar: ' https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirhttps://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/836.jpgLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1195.jpg',
+    pass: '$2a$10$sJ2lMjdUrUfDOdSrEtaTbukfB4Juh2/2WHDwiU9qM.FnGLdcPGepm',
+  })
+
+  console.log('inserted 1 document', result);
+
   //client.db().admin().listDatabases().then(result =>{
   //  console.log(result['databases'][6]);
   //})
@@ -20,8 +29,8 @@ client.connect(err => {
   //  console.log(result);
   //})
 
-  client.db('sample_training').collection('trips').find({'birth year': 1972}).toArray().then(result=>{
-    console.log(result);
-  })
+  //client.db('sample_training').collection('trips').find({'birth year': 1972}).toArray().then(result=>{
+  //  console.log(result);
+  //})
 
 });
